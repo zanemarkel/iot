d7c71a74ab16b23dcc138ab3bb6c2c5599b6833b
respectotron <zanemarkel@gmail.com>
2015-08-27 13:36:34 -0400

    johnn-five blink

---
+++ thinglabs/lab01.js
@@ -0,0 +1,32 @@
+//var five = require("johnny-five");
+//var board = new five.Board();
+//var LEDPIN = 13;
+//
+//board.on("ready", function(){
+//    // Set the LED to output mode
+//    this.pinMode(LEDPIN, five.Pin.OUTPUT);
+//
+//    // Create a flash/blink/strobe loop on led
+//    var val = 0;
+//    this.loop( 1000, function(){
+//        this.digitalWrite(LEDPIN, (val = val ? 0 : 1) );
+//    });
+//});
+
+var five = require("johnny-five");
+var board = new five.Board();
+var LEDPIN = 13;
+
+board.on("ready", function(){
+      
+    // Set pin 13 to OUTPUT mode
+    this.pinMode(LEDPIN, five.Pin.OUTPUT);
+
+    // Create a loop to "flash/blink/strobe" an led
+    var val = 0;
+
+    this.loop( 1000, function() {
+        this.digitalWrite(LEDPIN, (val = val ? 0 : 1));
+    });
+
+});
